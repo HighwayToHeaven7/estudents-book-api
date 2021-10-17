7e1d8a0354dff141ea34bd8b0586e69363dde201
Mateusz Jurek <mateusz.jurek@comarch.com>
2021-10-17 16:58:10 +0200

    first commit

---
+++ src/test/java/com/highwaytoheaven/estudentsbookapi/EstudentsbookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.highwaytoheaven.estudentsbookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudentsbookApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
