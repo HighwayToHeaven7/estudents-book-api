b0e79af3bafb924e104d6a695091fe35ade952fc
Mateusz Jurek <mateusz.jurek@comarch.com>
2022-01-07 13:28:33 +0100

    Uptime expiration jwt token.

--- src/main/java/com/highwaytoheaven/estudentsbookapi/infrastructure/security/jwt/JWTConstants.java
+++ src/main/java/com/highwaytoheaven/estudentsbookapi/infrastructure/security/jwt/JWTConstants.java
@@ -5,7 +5,7 @@ public class JWTConstants {
     public static final String HEADER = "Authorization";
     public static final String PREFIX = "Bearer ";
     public static final String SECRET_KEY = "sample_secret_key";
-    public static final int TIME = 900_000;
+    public static final int TIME = 9000_000;
     public static final String ROLE = "role";
     public static final String ID = "uuid";
 }
